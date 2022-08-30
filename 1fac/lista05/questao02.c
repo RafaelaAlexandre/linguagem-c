c4fc98cfe2a4092acda6c55283e8dd919a822756
rafaela-ale-oliveira <103013509+rafaela-ale-oliveira@users.noreply.github.com>
2022-08-30 13:22:43 -0300

    questao 2 lista 5 de fac

---
+++ 1fac/lista05/questao02.c
@@ -0,0 +1,20 @@
+/*****************************************
+				QUESTÃO 02: 
+Desenvolver um programa que calcule a soma 
+dos números de 1 a N, sendo N um número 
+inteiro fornecido pelo usuário.
+******************************************/
+#include<stdio.h>
+
+int main(){
+	int n, i,soma=0;
+
+	printf("entre com N: ");
+	scanf("%d",&n);
+
+	for(i=1;i<n;i++){
+		
+		soma+=i;
+	}
+	printf("soma: %d ",soma);	
+}
