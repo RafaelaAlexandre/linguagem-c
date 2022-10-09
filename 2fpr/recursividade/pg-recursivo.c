97ec065978294ff7ad5ac9cd66537271422eeead
rafaela-ale-oliveira <103013509+rafaela-ale-oliveira@users.noreply.github.com>
2022-10-09 20:34:24 -0300

    PG recursivo

---
+++ 2fpr/recursividade/pg-recursivo.c
@@ -0,0 +1,33 @@
+/*
+	Questão 04: Pede-se a implementação de uma
+	função recursiva que exiba os n primeiros
+	termos de uma PG (Progressão Geométrica),
+	onde a1 é o seu primeiro termo e q a razão.
+	Observação: uma PG consiste em uma
+	sequência de valores, iniciadas pelo valor a1. Os
+	demais elementos são definidos como o
+	anterior multiplicado pela razão.
+*/
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+void exibirPG(int n, int a1, int q );
+
+int main(){
+	
+	exibirPG(4,2,3);
+}
+void exibirPG(int n, int a1, int q){
+		if(n==1){
+			
+			printf(" %d ", a1);
+			
+		}else{
+			printf(" %d ", a1);		
+			
+			exibirPG(n-1, a1*q,q);
+			
+		
+		}		
+}
